0624510914568004f239944ba14828f775f86702
realestatelive <liverealestate28@gmail.com>
2025-06-12 18:46:29 +0000

    إضافة خلفية متغيرة ومتحركة (animated gradient) لواجهة الموقع لجعل الثيم عالمي واحترافي

--- realestate-app/pages/index.tsx
+++ realestate-app/pages/index.tsx
@@ -58,6 +58,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="animatedBg" />
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
